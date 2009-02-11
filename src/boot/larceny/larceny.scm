49658bcecbc33cad8eb80343386f88fd2b1ddc11
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-02-10 20:50:33 -0600

    boot/larceny: Update for err5rs mode

--- src/boot/larceny/larceny.scm
+++ src/boot/larceny/larceny.scm
@@ -1,4 +1,14 @@
 
+(import
+
+ (rnrs base)
+ (rnrs io simple)
+ (err5rs records syntactic)
+ (err5rs load)
+ (primitives current-directory do file-exists?)
+        
+ )
+
 (define scheme-implementation 'larceny)
 
 (load "src/boot/boot.scm")
