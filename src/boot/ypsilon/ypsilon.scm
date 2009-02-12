a824b721626a43b660e8e1ee3715756cf6b00647
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-02-12 09:45:10 -0600

    boot/ypsilon: add a 'load' procedure which handles extensionless files

--- src/boot/ypsilon/ypsilon.scm
+++ src/boot/ypsilon/ypsilon.scm
@@ -3,4 +3,11 @@
 
 (define scheme-implementation 'ypsilon)
 
+(define original-load load)
+
+(define (load file)
+  (if (file-exists? file)
+      (original-load file)
+      (original-load (string-append file ".scm"))))
+
 (load "src/boot/boot.scm")
