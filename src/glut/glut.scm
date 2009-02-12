ddba29832ed1f5893d997f906616c152f218a5b6
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-02-12 09:44:50 -0600

    glut: tie to chicken backend

--- src/glut/glut.scm
+++ src/glut/glut.scm
@@ -3,9 +3,9 @@
 
   ((ypsilon) ((-> loader 'lib) "glut/ypsilon"))
   
-  ;; ((larceny) (load "/scratch/_glut-larceny-a.scm"))
-
   ((larceny) ((-> loader 'lib) "glut/larceny"))
+
+  ((chicken) ((-> loader 'lib) "glut/chicken"))
   
   )
 
