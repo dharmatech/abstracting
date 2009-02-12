0be9e222a97719473e57d9cd209b83a5e71015c0
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-02-12 01:03:29 -0600

    glo: use color/rgba

--- src/glo/glo.scm
+++ src/glo/glo.scm
@@ -5,6 +5,8 @@
 
 ((-> loader 'lib) "glut")
 
+((-> loader 'lib) "color/rgba")
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (v- a b)
@@ -12,29 +14,6 @@
 	  (-> b 'first))
        (- (-> a 'second)
 	  (-> b 'second))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define (rgba red green blue alpha)
-
-  (let ((message-handler
-
-	 (lambda (msg)
-
-	   (case msg
-
-	     ((red)   red)
-	     ((green) green)
-	     ((blue)  blue)
-	     ((alpha) alpha)
-
-	     ((call-on-components)
-	      (lambda (procedure)
-		(procedure red green blue alpha)))
-
-	     ((raw) (vector red green blue alpha))))))
-
-    (vector 'rgba #f message-handler)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
