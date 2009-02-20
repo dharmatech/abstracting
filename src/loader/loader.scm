7ba21050c988ad69ed8211f142d34e6a8cb19834
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-02-20 13:14:00 -0600

    loader: trying some experiments

--- src/loader/loader.scm
+++ src/loader/loader.scm
@@ -17,6 +17,54 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; (define (make-loader)
+
+;;   (let ((roots (vec)))
+
+;;     (let ((message-handler
+
+;; 	   (lambda (msg)
+
+;; 	     (case msg
+
+;; 	       ((add-root)
+;; 		(lambda (path)
+;; 		  (set! roots ((-> roots 'suffix) path))))
+
+;;                ((roots) roots)
+	       
+;; 	       ((lib)
+;; 		(lambda (name)
+
+;; 		  (let ((lib-base-name
+;; 			 (let ((i (string-index-from-end name #\/)))
+;; 			   (if i
+;; 			       (substring name (+ i 1) (string-length name))
+;; 			       name))))
+                    
+;;                     (let ((relative-lib-file
+;;                            (string-append name "/" lib-base-name ".scm"))
+
+;;                           (relative-lib-file-short
+;;                            (string-append name "/" lib-base-name)))
+
+;;                       (let ((root ((-> roots 'find)
+;;                                    (lambda (root)
+;;                                      (-> (path (string-append root
+;;                                                               "/"
+;;                                                               relative-lib-file))
+;;                                          'exists?)))))
+;;                         (if root
+;;                             (load (string-append root "/" relative-lib-file))
+
+;;                             #f))))))
+
+;; 	       ))))
+
+;;       (vector 'loader #f message-handler))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define (make-loader)
 
   (let ((roots (vec)))
@@ -30,6 +78,8 @@
 	       ((add-root)
 		(lambda (path)
 		  (set! roots ((-> roots 'suffix) path))))
+
+               ((roots) roots)
 	       
 	       ((lib)
 		(lambda (name)
@@ -39,26 +89,48 @@
 			   (if i
 			       (substring name (+ i 1) (string-length name))
 			       name))))
-		  
-		  (let ((relative-lib-file
-                         (string-append name "/" lib-base-name ".scm"))
+                    
+                    (let ((relative-lib-file
+                           (string-append name "/" lib-base-name ".scm"))
 
-                        (relative-lib-file-short
-                         (string-append name "/" lib-base-name)))
+                          (relative-lib-file-short
+                           (string-append name "/" lib-base-name)))
 
-		    (let ((root ((-> roots 'find)
-				 (lambda (root)
-				   (-> (path (string-append root
-							    "/"
-							    relative-lib-file))
-				       'exists?)))))
-		      (if root
-			  (load (string-append root "/" relative-lib-file))
-                          ;; (load (string-append root "/" relative-lib-file-short))
-			  #f))))))
+                      (let ((root ((-> roots 'find)
+                                   (lambda (root)
+                                     (-> (path (string-append root
+                                                              "/"
+                                                              relative-lib-file))
+                                         'exists?)))))
+                        (if root
+                            (load (string-append root "/" relative-lib-file))
+                            #f))))))
+
+               ((lib-source-file)
+		(lambda (name)
+
+		  (let ((lib-base-name
+			 (let ((i (string-index-from-end name #\/)))
+			   (if i
+			       (substring name (+ i 1) (string-length name))
+			       name))))
+                    
+                    (let ((relative-lib-file
+                           (string-append name "/" lib-base-name ".scm"))
+
+                          (relative-lib-file-short
+                           (string-append name "/" lib-base-name)))
+
+                      (let ((root ((-> roots 'find)
+                                   (lambda (root)
+                                     (-> (path (string-append root
+                                                              "/"
+                                                              relative-lib-file))
+                                         'exists?)))))
+                        (if root
+                            (string-append root "/" relative-lib-file)
+                            #f))))))
 
 	       ))))
 
       (vector 'loader #f message-handler))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
