26586342b2560661b6572f6061ddbd9827c8eb13
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-02-20 13:14:45 -0600

    boot/larceny: Add 'define-macro' and a bunch of other things...

--- src/boot/larceny/larceny.scm
+++ src/boot/larceny/larceny.scm
@@ -1,14 +1,87 @@
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (import
 
- (rnrs base)
+ (for (rnrs base) run expand)
+
+ (for (rnrs syntax-case) run expand)
+
+ (for (rnrs bytevectors) run expand)
+
+ (for (rnrs control) run expand)
+
  (rnrs io simple)
  (err5rs records syntactic)
  (err5rs load)
- (primitives current-directory do file-exists?)
-        
+ (primitives current-directory file-exists? time)
+ 
  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-syntax define-macro
+   (lambda (x)
+     (syntax-case x ()
+       ((_ (name . args) . body)
+        (syntax (define-macro name (lambda args . body))))
+       ((_ name transformer)
+        (syntax
+         (define-syntax name
+           (lambda (y)
+             (syntax-case y ()
+                ((_ . args)
+                 (datum->syntax
+                  (syntax _)
+                  (apply transformer
+                         (syntax->datum (syntax args)))))))))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define exact->inexact inexact)
+
+(define inexact->exact exact)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define scheme-implementation 'larceny)
 
 (load "src/boot/boot.scm")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(import (primitives r5rs:require run-with-profiling
+
+                    reset-profiler!
+                    start-profiler!
+                    stop-profiler!
+                    report-profiler!
+
+                    ))
+
+(r5rs:require 'profile)
+
+(define (run-with-profiling* thunk)
+  (define val #f)
+  (if (not (procedure? thunk))
+      (assertion-violation 'run-with-profiling
+                           "argument should be thunk"
+                           thunk))
+  (reset-profiler!)
+  (start-profiler!)
+  (set! val (thunk))
+  (stop-profiler!)
+  (report-profiler!)
+
+  val)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-macro (profile expr)
+  `(run-with-profiling* (lambda () ,expr)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (inc name)
+  ((-> loader 'lib) name))
