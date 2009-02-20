1e3f5192b7f90d106427ed1c1d28c78440887809
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-02-20 13:13:30 -0600

    combinators: add eq-to? and equal-to?

--- src/combinators/combinators.scm
+++ src/combinators/combinators.scm
@@ -1,2 +1,11 @@
 
 (define (partial-apply-a-b f) (lambda (a) (lambda (b) (f a b))))
+
+(define (eq-to? a)
+  (lambda (b)
+    (eq? a b)))
+
+(define (equal-to? a)
+  (lambda (b)
+    (equal? a b)))
+
