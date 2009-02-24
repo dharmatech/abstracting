f41a12573ef03a417b5972b66eac2cac4d0dbc51
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-02-24 04:08:05 -0600

    gl/chicken: Simplify: the opengl egg now accepts blogs for functions which want GLdouble* arguments

--- src/gl/chicken/chicken.scm
+++ src/gl/chicken/chicken.scm
@@ -1,23 +1,3 @@
 
 (use gl)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define _glGetDoublev glGetDoublev)
-
-(define (glGetDoublev val bv)
-  (_glGetDoublev val (blob->f64vector/shared bv)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define _glLoadMatrixd glLoadMatrixd)
-
-(define (glLoadMatrixd bv)
-  (_glLoadMatrixd (blob->f64vector/shared bv)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define _glMultMatrixd glMultMatrixd)
-
-(define (glMultMatrixd bv)
-  (_glMultMatrixd (blob->f64vector/shared bv)))
