63214b638c2ee188f0df90e440f3fb2ae145ede1
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-03-08 07:11:20 -0500

    Add 'xlib/chicken'

---
+++ ext/xlib/chicken/chicken.scm
@@ -0,0 +1,9 @@
+
+;; (chicken-scheme-load
+;;  (string-append abstracting-root-directory
+;;                 "/support/chicken/xlib-bytevectors/xlib-bytevectors.so"))
+
+(chicken-scheme-load
+ (string-append abstracting-root-directory
+                "/support/chicken/xlib/xlib.so"))
+
