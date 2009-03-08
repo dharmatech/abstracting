1506a6d07877c10345496a60cb4f38c568780ebf
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-03-08 07:09:42 -0500

    Add 'hvec' and 'srfi-4' based implementation

---
+++ src/hvec/hvec.scm
@@ -0,0 +1,8 @@
+
+(case scheme-implementation
+
+  ((chicken ypsilon) (: loader 'lib "hvec/srfi-4"))
+
+  ;; ((ypsilon) (: loader 'lib "hvec/r6rs-bytevectors"))
+
+  )
---
+++ src/hvec/srfi-4/srfi-4.scm
@@ -0,0 +1,82 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(: loader 'lib "srfi/4")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (s32-vec-obj hv)
+
+  (let ((len (lambda (     ) (s32vector-length hv      )))
+        (ref (lambda (i    ) (s32vector-ref    hv i    )))
+        (set (lambda (i val) (s32vector-set!   hv i val))))
+
+    (let ((message-handler
+
+           (lambda (msg)
+
+             (case msg
+
+               ((len) len)
+               ((ref) ref)
+               ((set) set)
+               ((raw) (lambda () hv))))))
+
+      (vector 's32-vec hv message-handler))))
+
+(define (s32-vec-of-len n) (make-s32vector n 0.0))
+
+(define (s32-vec . elts) (s32-vec-obj (apply s32vector elts)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (u32-vec-obj hv)
+
+  (let ((len (lambda (     ) (u32vector-length hv      )))
+        (ref (lambda (i    ) (u32vector-ref    hv i    )))
+        (set (lambda (i val) (u32vector-set!   hv i val))))
+
+    (let ((message-handler
+
+           (lambda (msg)
+
+             (case msg
+
+               ((len) len)
+               ((ref) ref)
+               ((set) set)
+               ((raw) (lambda () hv))))))
+
+      (vector 'u32-vec hv message-handler))))
+
+(define (u32-vec-of-len n) (make-u32vector n 0.0))
+
+(define (u32-vec . elts) (u32-vec-obj (apply u32vector elts)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (f64-vec-obj hv)
+
+  (let ((len (lambda (     ) (f64vector-length hv      )))
+        (ref (lambda (i    ) (f64vector-ref    hv i    )))
+        (set (lambda (i val) (f64vector-set!   hv i val))))
+
+    (let ((message-handler
+
+           (lambda (msg)
+
+             (case msg
+
+               ((len) len)
+               ((ref) ref)
+               ((set) set)
+               ((raw) (lambda () hv))))))
+
+      (vector 'f64-vec hv message-handler))))
+
+(define (f64-vec-of-len n) (make-f64vector n 0.0))
+
+(define (f64-vec . elts) (f64-vec-obj (apply f64vector elts)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
