0d4e6437edab4b1c109b2b61761dfab69dfe0471
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-03-21 07:58:06 -0500

    Rewrite chicken 'inc' macro

--- src/boot/chicken/inc/inc.scm
+++ src/boot/chicken/inc/inc.scm
@@ -1,39 +1,14 @@
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use srfi-13)
 
-(define (string-index-from-end-start-at string char i)
-  
-  (cond
+(define (lib-base-name name)
+  (cond ((string-index-right name #\/) =>
+         (lambda (i)
+           (string-drop name (+ i 1))))
+        (else name)))
 
-   ((< i 0) #f)
-
-   ((equal? (string-ref string i) char) i)
-
-   (else
-    (string-index-from-end-start-at string char (- i 1)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define (string-index-from-end string char)
-  (string-index-from-end-start-at string char (- (string-length string) 1)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define (relative-lib-file name)
-  (let ((lib-base-name
-         (let ((i (string-index-from-end name #\/)))
-           (if i
-               (substring name (+ i 1) (string-length name))
-               name))))
-    (string-append name "/" lib-base-name ".scm")))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (relative-source-file name)
+  (string-append name "/" (lib-base-name name) ".scm"))
 
 (define-macro (inc name)
-
-  (let ((source-file (relative-lib-file name)))
-
-    `(include ,source-file)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+  `(include ,(relative-source-file name)))
