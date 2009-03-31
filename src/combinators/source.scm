05fbdc747ca8efc5f431224040a4ed01a5171423
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-03-31 07:54:59 -0500

    combinators: add a bunch of curry and paritial application combinators

--- src/combinators/source.scm
+++ src/combinators/source.scm
@@ -29,3 +29,78 @@
   (lambda (b)
     (+ a b)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (curry/ab procedure)
+  (lambda (a)
+    (lambda (b)
+      (procedure a b))))
+
+(define (curry/ba procedure)
+  (lambda (b)
+    (lambda (a)
+      (procedure a b))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (curry/a:bc procedure)
+  (lambda (a)
+    (lambda (b c)
+      (procedure a b c))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (curry/b:ac procedure)
+  (lambda (b)
+    (lambda (a c)
+      (procedure a b c))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (curry/an procedure)
+  (lambda (a)
+    (lambda args
+      (apply procedure a args))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (partial-apply/ab procedure a)
+  (lambda (b)
+    (procedure a b)))
+
+(define (partial-apply/ba procedure b)
+  (lambda (a)
+    (procedure a b)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (apply/ab f)
+  (lambda (a b)
+    (f a b)))
+
+(define (apply/ba f)
+  (lambda (a b)
+    (f b a)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; (define (bi x f g c)
+;;   (c (f x)
+;;      (g x)))
+
+;; (define (bi@ x y f c)
+;;   (c (f x)
+;;      (f y)))
+
+;; (define (bi* x y f g c)
+;;   (c (f x)
+;;      (g y)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; (define (2bi x y f g c)
+;;   (c (f x y)
+;;      (g x y)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
