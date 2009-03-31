c1ad7395f6ea4ca632db8a80cfb2db63ed04ca86
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-03-31 07:48:01 -0500

    add hsh

---
+++ src/hsh/source.scm
@@ -0,0 +1,44 @@
+
+(define (hsh-obj table)
+
+  (let ((message-handler
+
+         (lambda (msg)
+
+           (case msg
+
+             ((len)
+              (lambda ()
+                (hashtable-size table)))
+
+             ((ref)
+              (lambda (key)
+                (hashtable-ref table key #f)))
+
+             ((set)
+              (lambda (key val)
+                (hashtable-set! table key val)))
+
+             ((delete)
+              (lambda (key)
+                (hashtable-delete! table key)))
+
+             ((contains?)
+              (lambda (key)
+                (hashtable-contains? table key)))
+
+             ((clone)
+              (lambda ()
+                (hashtable-copy #t)))
+
+             ((clear)
+              (lambda ()
+                (hashtable-clear! table)))
+
+             ((keys)
+              (lambda ()
+                (hashtable-keys table)))
+
+             ))))
+
+    (vector 'hashtable table message-handler)))
