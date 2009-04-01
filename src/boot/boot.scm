e46b88ce4b08fcdd1842893dc32d2ff87fa633c3
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-04-01 12:39:14 -0500

    boot: add experimental root

--- src/boot/boot.scm
+++ src/boot/boot.scm
@@ -3,5 +3,6 @@
 
 (set! *roots* (list (string-append abstracting-root-directory "/src")
                     (string-append abstracting-root-directory "/ext")
-                    (string-append abstracting-root-directory "/examples")))
+                    (string-append abstracting-root-directory "/examples")
+                    (string-append abstracting-root-directory "/experimental")))
 
