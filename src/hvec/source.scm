be61c0b95943259a8e36358dead641b9c86f5acd
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-04-02 18:04:03 -0500

    hvec: add ikarus

--- src/hvec/source.scm
+++ src/hvec/source.scm
@@ -3,7 +3,7 @@
 
   ;; ((chicken ypsilon larceny gambit) (: loader 'lib "hvec/srfi-4"))
 
-  ((chicken ypsilon larceny gambit) (require-lib "hvec/srfi-4"))
+  ((chicken ypsilon larceny gambit ikarus) (require-lib "hvec/srfi-4"))
 
   ;; ((ypsilon) (: loader 'lib "hvec/r6rs-bytevectors"))
 
