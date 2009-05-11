a197f3e7f2446db33d9a11fa913a1e49c2118d13
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-05-10 21:57:19 -0500

    flexi-line: Rewrite to use glamour. Trying out flatter structure.

--- examples/flexi-line/source.scm
+++ examples/flexi-line/source.scm
@@ -1,4 +1,6 @@
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Original version by Jill Jackson
 ;;
 ;; http://www.openprocessing.org/visuals/?visualID=323
@@ -9,127 +11,79 @@
 
 (initialize-glut)
 
-(let (
+(glutInitDisplayMode GLUT_DOUBLE)
+  
+(basic-window "Flexi Line by Jill Jackson" (width 500) (height 500))
 
-      (width  500)
-      (height 500)
+(mouse mouse-button mouse-state mouse-x mouse-y)
 
-      (mouse-button 0)
-      (mouse-state  GLUT_UP)
-      (mouse-x      0)
-      (mouse-y      0)
+(frames-per-second 30)
 
-      )
+(define make-point
 
-  (glutInitDisplayMode GLUT_DOUBLE)
+  (let ((pow 1))
+    
+    (lambda (x y)
+      
+      (let ((dx 0)
+            (dy 0))
 
-  (glutInitWindowSize width height)
+        (let ((update
+               (lambda ()
 
-  (glutCreateWindow "Flexi Line by Jill Jackson")
+                 ;; accelerate
 
-  (glutPassiveMotionFunc
-   (lambda (x y)
-     (set! mouse-x x)
-     (set! mouse-y y)))
-
-  (glutMouseFunc
-   (lambda (button state x y)
-     (set! mouse-button button)
-     (set! mouse-state  state)
-     (set! mouse-x      x)
-     (set! mouse-y      y)))
-
-  (frames-per-second 30)
-
-  (glutReshapeFunc
-   (lambda (w h)
-
-     (glViewport 0 0 w h)
-
-     (glMatrixMode GL_PROJECTION)
-
-     (glLoadIdentity)
-
-     (glOrtho 0.0 (inexact w) (inexact h) 0.0 -1000.0 1000.0)
-     
-     (set! width  w)
-     (set! height h)))
-       
-  (let ((update-points
-
-         (let ((points
-
-                (let ((make-point
-                       
-                       (let ((pow 1))
-                         
-                         (lambda (x y)
+                 (if (= mouse-state GLUT_DOWN)
+                     (let ((k (* pow
+                                 pow
+                                 (if (= mouse-button
+                                        GLUT_LEFT_BUTTON)
+                                     1
+                                     -1)))
                            
-                           (let ((dx 0)
-                                 (dy 0))
+                           (ang (atan (- mouse-y y)
+                                      (- mouse-x x))))
 
-                             (let ((update
-                                    (lambda ()
+                       (set! dx (+ dx (* k (cos ang))))
+                       (set! dy (+ dy (* k (sin ang))))))
 
-                                      ;; accelerate
+                 ;; move
 
-                                      (if (= mouse-state GLUT_DOWN)
-                                          (let ((k (* pow
-                                                      pow
-                                                      (if (= mouse-button
-                                                             GLUT_LEFT_BUTTON)
-                                                          1
-                                                          -1)))
-                                                
-                                                (ang (atan (- mouse-y y)
-                                                           (- mouse-x x))))
+                 (set! dx (* dx 0.98))
+                 (set! dy (* dy 0.98))
 
-                                            (set! dx (+ dx (* k (cos ang))))
-                                            (set! dy (+ dy (* k (sin ang))))))
+                 (set! x (+ x dx))
+                 (set! y (+ y dy))
 
-                                      ;; move
+                 ;; draw
 
-                                      (set! dx (* dx 0.98))
-                                      (set! dy (* dy 0.98))
+                 (glVertex2d x y))))
 
-                                      (set! x (+ x dx))
-                                      (set! y (+ y dy))
+          (vector 'point update))))))
 
-                                      ;; draw
+(define (update-point p)
+  ((vector-ref p 1)))
 
-                                      (glVertex2d x y))))
+(define num 5000)
 
-                               (vector 'point update))))))
-                      
-                      (num 5000))
+(define points
+  (list-tabulate num
+                 (lambda (i)
+                   (make-point (inexact (* (/ i num) width))
+                               (inexact (/ height 2))))))
 
-                  (list-tabulate num
-                                 (lambda (i)
-                                   (make-point (* (inexact (/ i num)) width)
-                                               (inexact (/ height 2)))))))
+(define (update-points)
+  (for-each update-point points))
 
-               (update-point
-                (lambda (p)
-                  ((vector-ref p 1)))))
+(buffered-display-procedure
+ (lambda ()
 
-           (lambda ()
-             (for-each update-point points)))))
+   (background 0.0)
 
-    (glutDisplayFunc
-     (lambda ()
-     
-       (glMatrixMode GL_MODELVIEW)
-       (glLoadIdentity)
-
-       (glClearColor 0.0 0.0 0.0 0.0)
-
-       (glClear GL_COLOR_BUFFER_BIT)
-
-       (glColor4d 1.0 1.0 1.0 1.0)
-       (glBegin GL_LINE_STRIP)
-       (update-points)
-       (glEnd)
-
-       (glutSwapBuffers)))))
+   (glColor4d 1.0 1.0 1.0 1.0)
+   
+   (glBegin GL_LINE_STRIP)
+   (update-points)
+   (glEnd) ))
 
 (glutMainLoop)
